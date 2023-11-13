dc4100ff4d809a0fb8c5685f91f355f86e79eda3
Tania Zubrienko <tatiana.zubrienko@gmail.com>
2023-11-13 15:33:31 +0100

    profile routes

--- routes/carers.routes.js
+++ routes/carers.routes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const User = require("./../models/User.model")
+
 //GET listado
 router.get('/cuidadores-lista', (req, res) => {
     User
