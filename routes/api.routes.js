59bc75201b6f62f6c7b4654f2e76f79faba79a5e
“Raquel <raquel.torre.oporto@gmail.com>
2023-11-14 18:45:27 +0100

    working in carer details map

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -11,5 +11,12 @@ router.get("/carers-location", (req, res, next) => {
         .catch(err => res.status(500).json({ message: "Server error", errorDetails: err }))
 })
 //userlist GET
+router.get("/carers-location/:carer_id", (req, res, next) => {
+    const { carer_id } = req.params
+    User
+        .findById(carer_id)
+        .then(result => res.json(result))
+        .catch(err => res.status(500).json({ message: "Server error", errorDetails: err }))
+})
 
 module.exports = router;
