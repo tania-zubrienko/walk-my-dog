572b7ddddf49ee838c97044a3819aafcf2aea302
Tania Zubrienko <tatiana.zubrienko@gmail.com>
2023-11-14 12:38:54 +0100

    carers map render

--- public/js/carers.js
+++
@@ -1,9 +0,0 @@
-let myMap
-let userLocation = { lat: 40.44699825339554, lng: -3.6751472005642563 }
-
-const carersButton = document.querySelector("#carers")
-pButton.addEventListener('click', () => renderCarersMap())
-
-function renderCarersMap() {
-    console.log("ESOTY EN OTRO SCRIPT")
-}
